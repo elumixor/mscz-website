66d28928c58f50271698d088652e4923c5eb9ec4
elumixor <vladogim97@gmail.com>
2024-04-09 14:32:02 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Boilerplate for angular projects
+
+Built with Angular.
---
+++ src/app/app.config.ts
@@ -0,0 +1,9 @@
+import type { ApplicationConfig } from "@angular/core";
+import { provideRouter } from "@angular/router";
+
+import { routes } from "./app.routes";
+import { provideClientHydration } from "@angular/platform-browser";
+
+export const appConfig: ApplicationConfig = {
+    providers: [provideRouter(routes), provideClientHydration()],
+};
