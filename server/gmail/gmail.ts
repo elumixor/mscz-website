0c36aa1336ca538387f2061491651d3dc30e7ccf
elumixor <vladogim97@gmail.com>
2024-05-06 16:08:30 +0200

    Changed google authorization to google password

--- server/gmail/gmail.ts
+++ server/gmail/gmail.ts
@@ -1,28 +1,28 @@
-import { EventEmitter } from "@elumixor/frontils";
-import type { JWTInput, OAuth2Client } from "google-auth-library";
-import { gmail_v1, google } from "googleapis";
 import juice from "juice";
 import MailComposer from "nodemailer/lib/mail-composer";
 import type Mail from "nodemailer/lib/mailer";
 import path from "path";
 import { fileData } from "../utils";
+import nodemailer from "nodemailer";
 
 export class Gmail {
-    private readonly authenticated = new EventEmitter<gmail_v1.Gmail>();
     // If modifying these scopes, delete token.json.
-    private readonly token = fileData<JWTInput>("secret/google.token.json");
-
-    readonly gmail = this.authenticated.nextEvent;
-
-    constructor() {
-        this.authorize();
-    }
+    private readonly password = fileData<string>("secret/google.json", { required: true });
+    private readonly transporter = nodemailer.createTransport({
+        service: "Gmail",
+        host: "smtp.gmail.com",
+        port: 465,
+        secure: true,
+        auth: {
+            user: "meditationsteps.czechia@gmail.com",
+            pass: this.password(),
+        },
+    });
 
     async send(options: {
         to: string;
         subject: string;
         html: string;
-        text?: string;
         attachments?: Mail.Attachment[];
         replacements?: Record<string, string>;
     }) {
@@ -30,33 +30,24 @@ export class Gmail {
         options.html = html;
         options.attachments = [...(options.attachments ?? []), ...attachments];
 
-        const mail = new MailComposer(options);
-        const message = await mail.compile().build();
-        const encodedMessage = Buffer.from(message)
-            .toString("base64")
-            .replace(/\+/g, "-")
-            .replace(/\//g, "_")
-            .replace(/=+$/, "");
-
-        const gmail = await this.gmail;
-
         // eslint-disable-next-line no-console
         console.log("Sending email to", options.to);
-        await gmail.users.messages.send({
-            userId: "me",
-            requestBody: {
-                raw: encodedMessage,
-            },
+
+        return new Promise((res, rej) => {
+            this.transporter.sendMail(
+                {
+                    from: "me",
+                    to: options.to,
+                    subject: options.subject,
+                    html: options.html,
+                    attachments: options.attachments,
+                },
+                (error, info) => {
+                    if (error) rej(error);
+                    else res(info.response);
+                },
+            );
         });
-    }
-
-    private authorize() {
-        const token = this.token();
-        if (!token) throw new Error("No token found. Call 'npm run gmail' to authenticate.");
-
-        const auth = google.auth.fromJSON(token) as OAuth2Client;
-        const gmail = google.gmail({ version: "v1", auth });
-        this.authenticated.emit(gmail);
     }
 
     private processHTML(html: string, replacements?: Record<string, string>) {
@@ -76,7 +67,7 @@ export class Gmail {
             const attachment = {
                 filename: path.basename(extracted),
                 path: `.${extracted}`,
-                cid: `attachment${i}`,
+                cid: `attachment${String(i)}`,
             };
             html = html.replace(src, `src="cid:${attachment.cid}"`);
             attachments.push(attachment);
